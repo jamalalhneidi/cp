dd63aab9b8330549eed7f010833b3b09ebf7f96c
jamal <jamal.alhneidi@gmail.com>
2023-08-08 23:36:59 +0200

    updated UF template

--- templates/data structures/UF.cpp
+++ templates/data structures/UF.cpp
@@ -13,20 +13,25 @@ struct UF {
         return p[u] == u ? u : p[u] = find(p[u]);
     }
 
-    void join(int u, int v) {
+    bool join(int u, int v) {
         u = find(u), v = find(v);
-        if (u == v)return;
+        if (u == v)return false;
         if (size[u] < size[v])swap(u, v);
         p[v] = u;
         size[u] += size[v];
         --cc;
+        return true;
+    }
+
+    bool connected(int u, int v) {
+        return find(u) == find(v);
     }
 
     int getSize(int u) {
         return size[find(u)];
     }
 
-    int getComponents() const {
+    int getCC() const {
         return cc;
     }
 
