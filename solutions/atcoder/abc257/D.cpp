f44c4a2e3e80fe0c0aff964522ab6069faba8f2c
Jamal Alhneidi <jamal.alhneidi@gmail.com>
2023-12-26 15:31:41 +0100

    sol abc257 D

---
+++ solutions/atcoder/abc257/D.cpp
@@ -0,0 +1,144 @@
+/*
+https://atcoder.jp/contests/abc257/tasks/abc257_d
+
+binary search
+graphs
+floyd-warshall
+
+
+*dfs instead of floyd-warshall is much faster here
+    https://atcoder.jp/contests/abc257/submissions/48869653
+
+Take aways:
+1. binary search bounds
+2. overflow in calculation or return type of functions
+
+ */
+
+#include "bits/stdc++.h"
+
+#ifdef LOCAL
+
+#include "templates/others/dbg.cpp"
+
+#else
+#define dbg(...)
+#define dbgln(...)
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef queue<int> qi;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vector<pll>> vvpll;
+typedef vector<char> vc;
+typedef vector<vector<char>> vvc;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+#define endl '\n'
+#define all(a) a.begin(), a.end()
+#define yes "YES"
+#define no "NO"
+#define spc ' '
+#define INF INT_MAX
+#define LLINF LONG_LONG_MAX
+#define NINF INT_MIN
+#define LLNINF LONG_LONG_MIN
+#define pb push_back
+#define pf push_front
+#define eb emplace_back
+#define F first
+#define S second
+#define cin(a) \
+    for (auto &_x : (a)) cin >> _x
+#define cin2(a)          \
+    for (auto &_x : (a)) \
+        for (auto &__x : _x) cin >> __x
+#define cout(a) \
+    for (auto &_x : (a)) cout << _x << spc
+#define len(l, r) ((r) - (l) + 1)
+#define mid(l, r) ((l) + ((r) - (l)) / 2)
+#define ceil(a, b) (!(a) ? 0 : ((a)-1) / (b) + 1)
+#define yn(b) ((b) ? yes : no)
+#define between(x, l, r) ((x) >= (l) && (x) <= (r))
+#define between2(i, j, n, m) (between(i, 0, n - 1) && between(j, 0, m - 1))
+
+struct Point {
+    ll x, y, p;
+
+    ll dist(Point &that) {
+        return abs(x - that.x) + abs(y - that.y);
+    }
+
+    bool canReach(Point &that, ll s) {
+        return p * s >= dist(that);
+    }
+};
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<Point> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i].x >> a[i].y >> a[i].p;
+    }
+    auto f = [&](ll s) {
+        vvi d(n, vi(n));
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == j) continue;
+                d[i][j] = a[i].canReach(a[j], s);
+            }
+        }
+        for (int k = 0; k < n; ++k) {
+            for (int i = 0; i < n; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (i == j) continue;
+                    d[i][j] |= d[i][k] & d[k][j];
+                }
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (count(all(d[i]), 0) == 1) return true;
+        }
+        return false;
+    };
+    ll l = -1, r = 1e10;
+    while (r - l > 1) {
+        ll m = mid(l, r);
+        if (f(m))
+            r = m;
+        else
+            l = m;
+    }
+    cout << r;
+}
+
+int main() {
+    fast;
+    int t = 1;
+#ifdef MULTI
+    cin >> t;
+#endif
+    for (int i = 1; i <= t; ++i) {
+        solve();
+        i != t &&cout << endl;
+    }
+}
+/*
+
+ */
