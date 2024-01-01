72b18e4cea07beb055ae2c33bb352f9a094ee366
Jamal Alhneidi <jamal.alhneidi@gmail.com>
2024-01-01 18:40:14 +0100

    sol mania de par

---
+++ solutions/acmicpc/2015 ICPC Brazil Subregional/Mania_de_Par.cpp
@@ -0,0 +1,127 @@
+/*
+
+Mania de par
+
+https://www.acmicpc.net/problem/13595
+
+graphs
+garph modeling
+shortest paths
+dijkstra
+
+ */
+
+#include "bits/stdc++.h"
+
+#ifdef LOCAL
+
+#include "templates/others/dbg.cpp"
+
+#else
+#define dbg(...)
+#define dbgln(...)
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef queue<int> qi;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vector<pll>> vvpll;
+typedef vector<char> vc;
+typedef vector<vector<char>> vvc;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+#define endl '\n'
+#define all(a) a.begin(), a.end()
+#define yes "YES"
+#define no "NO"
+#define spc ' '
+#define INF INT_MAX
+#define LLINF LONG_LONG_MAX
+#define NINF INT_MIN
+#define LLNINF LONG_LONG_MIN
+#define pb push_back
+#define pf push_front
+#define eb emplace_back
+#define F first
+#define S second
+#define cin(a) \
+    for (auto &_x : (a)) cin >> _x
+#define cin2(a)          \
+    for (auto &_x : (a)) \
+        for (auto &__x : _x) cin >> __x
+#define cout(a) \
+    for (auto &_x : (a)) cout << _x << spc
+#define len(l, r) ((r) - (l) + 1)
+#define mid(l, r) ((l) + ((r) - (l)) / 2)
+#define ceil(a, b) (!(a) ? 0 : ((a)-1) / (b) + 1)
+#define yn(b) ((b) ? yes : no)
+#define between(x, l, r) ((x) >= (l) && (x) <= (r))
+#define between2(i, j, n, m) (between(i, 0, n - 1) && between(j, 0, m - 1))
+
+struct E {
+    int v, w;
+};
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector adj(n, vector<E>());
+    for (int i = 0; i < m; ++i) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        --u, --v;
+        adj[u].pb({v, w});
+        adj[v].pb({u, w});
+    }
+    priority_queue<array<int, 3>, vector<array<int, 3>>, greater<>> pq;
+    vvi dist(n, vi(2, INF));
+    pq.push({0, 0, 0});
+    dist[0][0] = 0;
+    while (!pq.empty()) {
+        auto [d, u, p] = pq.top();
+        pq.pop();
+        if (d != dist[u][p]) continue;
+        for (auto [v, w] : adj[u]) {
+            if (d + w < dist[v][!p]) {
+                dist[v][!p] = d + w;
+                pq.push({dist[v][!p], v, !p});
+            }
+        }
+    }
+    if (dist[n - 1][0] == INF)
+        cout << -1;
+    else
+        cout << dist[n - 1][0];
+}
+
+int main() {
+    fast;
+    int t = 1;
+#ifdef MULTI
+    cin >> t;
+#endif
+    for (int i = 1; i <= t; ++i) {
+        solve();
+        i != t &&cout << endl;
+    }
+}
+/*
+
+
+
+ */
