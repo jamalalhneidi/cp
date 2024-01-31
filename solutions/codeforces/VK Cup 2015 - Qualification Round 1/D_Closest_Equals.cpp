056fed186e3a11c8ea424ad682b6aa001dd20744
Jamal Alhneidi <jamal.alhneidi@gmail.com>
2024-01-31 23:40:23 +0100

    sol cf vk2015 d

---
+++ solutions/codeforces/VK Cup 2015 - Qualification Round 1/D_Closest_Equals.cpp
@@ -0,0 +1,141 @@
+/*
+
+Closest Equals
+
+https://codeforces.com/problemset/problem/522/D
+
+segment tree (offline)
+
+*2000
+
+
+ */
+
+#include "bits/stdc++.h"
+
+#ifdef LOCAL
+
+#include "templates/others/dbg.cpp"
+
+#else
+#define dbg(...)
+#define dbgln(...)
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef queue<int> qi;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vector<pll>> vvpll;
+typedef vector<char> vc;
+typedef vector<vector<char>> vvc;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+#define endl '\n'
+#define all(a) a.begin(), a.end()
+#define yes "YES"
+#define no "NO"
+#define spc ' '
+#define INF INT_MAX
+#define LLINF LONG_LONG_MAX
+#define NINF INT_MIN
+#define LLNINF LONG_LONG_MIN
+#define pb push_back
+#define pf push_front
+#define eb emplace_back
+#define F first
+#define S second
+#define cin(a) \
+    for (auto &_x : (a)) cin >> _x
+#define cin2(a)          \
+    for (auto &_x : (a)) \
+        for (auto &__x : _x) cin >> __x
+#define cout(a) \
+    for (auto &_x : (a)) cout << _x << spc
+#define len(l, r) ((r) - (l) + 1)
+#define mid(l, r) ((l) + ((r) - (l)) / 2)
+#define ceil(a, b) (!(a) ? 0 : ((a)-1) / (b) + 1)
+#define yn(b) ((b) ? yes : no)
+#define between(x, l, r) ((x) >= (l) && (x) <= (r))
+#define between2(i, j, n, m) (between(i, 0, n - 1) && between(j, 0, m - 1))
+
+struct ST {
+    int n;
+    vi t;
+    ST(int n) {
+        this->n = n;
+        t.resize(n << 1, INF);
+    }
+    void set(int i, int x) {
+        i += n;
+        t[i] = x;
+        for (i >>= 1; i; i >>= 1) t[i] = min(t[i << 1], t[i << 1 | 1]);
+    }
+    int query(int l, int r) {
+        int res = INF;
+        for (l += n, r += n + 1; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) res = min(res, t[l++]);
+            if (r & 1) res = min(res, t[--r]);
+        }
+        return res;
+    }
+};
+
+void solve() {
+    int n, q;
+    cin >> n >> q;
+    vi a(n);
+    cin(a);
+    mii m;
+    vi nxt(n, -1);
+    for (int i = n - 1; i >= 0; --i) {
+        if (m.count(a[i])) nxt[i] = m[a[i]];
+        m[a[i]] = i;
+    }
+    vector<vpii> queries(n);
+    for (int i = 0; i < q; ++i) {
+        int l, r;
+        cin >> l >> r;
+        queries[l - 1].pb({r - 1, i});
+    }
+    ST st(n);
+    vi ans(q);
+    for (int i = n - 1; i >= 0; --i) {
+        if (nxt[i] != -1) st.set(nxt[i], nxt[i] - i);
+        for (auto [r, j] : queries[i]) {
+            ans[j] = st.query(i, r);
+        }
+    }
+    for (auto x : ans) cout << (x == INF ? -1 : x) << endl;
+}
+
+int main() {
+    fast;
+    int t = 1;
+#ifdef MULTI
+    cin >> t;
+#endif
+    for (int i = 1; i <= t; ++i) {
+        solve();
+        i != t &&cout << endl;
+    }
+}
+/*
+
+
+
+ */
