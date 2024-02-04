45b488bbfed8f841959489225c620e52036786d1
Jamal Alhneidi <jamal.alhneidi@gmail.com>
2024-02-04 20:50:55 +0100

    sol abc183 F

---
+++ solutions/atcoder/abc183/F.cpp
@@ -0,0 +1,158 @@
+/*
+
+Confluence
+
+https://atcoder.jp/contests/abc183/tasks/abc183_f
+
+UF
+
+ */
+
+#include "bits/stdc++.h"
+
+#ifdef LOCAL
+
+#include "templates/others/dbg.cpp"
+
+#else
+#define dbg(...)
+#define dbgln(...)
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef queue<int> qi;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vector<pll>> vvpll;
+typedef vector<char> vc;
+typedef vector<vector<char>> vvc;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+#define endl '\n'
+#define all(a) a.begin(), a.end()
+#define yes "YES"
+#define no "NO"
+#define spc ' '
+#define INF INT_MAX
+#define LLINF LONG_LONG_MAX
+#define NINF INT_MIN
+#define LLNINF LONG_LONG_MIN
+#define pb push_back
+#define pf push_front
+#define eb emplace_back
+#define F first
+#define S second
+#define cin(a) \
+    for (auto &_x : (a)) cin >> _x
+#define cin2(a)          \
+    for (auto &_x : (a)) \
+        for (auto &__x : _x) cin >> __x
+#define cout(a) \
+    for (auto &_x : (a)) cout << _x << spc
+#define len(l, r) ((r) - (l) + 1)
+#define mid(l, r) ((l) + ((r) - (l)) / 2)
+#define ceil(a, b) (!(a) ? 0 : ((a)-1) / (b) + 1)
+#define yn(b) ((b) ? yes : no)
+#define between(x, l, r) ((x) >= (l) && (x) <= (r))
+#define between2(i, j, n, m) (between(i, 0, n - 1) && between(j, 0, m - 1))
+
+struct UF {
+   private:
+    vi p, size;
+    int cc;
+
+   public:
+    UF(int n) {
+        p = vi(n);
+        size = vi(n, 1);
+        iota(all(p), 0);
+        cc = n;
+    }
+
+    int find(int u) {
+        return p[u] == u ? u : p[u] = find(p[u]);
+    }
+
+    bool join(int u, int v) {
+        u = find(u), v = find(v);
+        if (u == v) return false;
+        if (size[u] < size[v]) swap(u, v);
+        p[v] = u;
+        size[u] += size[v];
+        --cc;
+        return true;
+    }
+
+    bool connected(int u, int v) {
+        return find(u) == find(v);
+    }
+
+    int getSize(int u) {
+        return size[find(u)];
+    }
+
+    int getCC() const {
+        return cc;
+    }
+};
+
+void solve() {
+    int n, q;
+    cin >> n >> q;
+    vector<mii> m(n);
+    for (int i = 0; i < n; ++i) {
+        int c;
+        cin >> c;
+        m[i][c]++;
+    }
+    UF uf(n);
+    while (q--) {
+        int t;
+        cin >> t;
+        if (t == 1) {
+            int u, v;
+            cin >> u >> v;
+            --u, --v;
+            u = uf.find(u), v = uf.find(v);
+            if (!uf.join(u, v)) continue;
+            int p = uf.find(u);
+            if (u != p) swap(u, v);
+            for (auto &[k, cnt] : m[v]) m[u][k] += cnt;
+        } else {
+            int i, c;
+            cin >> i >> c;
+            cout << m[uf.find(i - 1)][c] << endl;
+        }
+    }
+}
+
+int main() {
+    fast;
+    int t = 1;
+#ifdef MULTI
+    cin >> t;
+#endif
+    for (int i = 1; i <= t; ++i) {
+        solve();
+        i != t &&cout << endl;
+    }
+}
+/*
+
+
+
+ */
