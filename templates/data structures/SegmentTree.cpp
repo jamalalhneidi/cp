768115f604a7cd11bad053e32b5bba25d1970c3e
Jamal Alhneidi <jamal.alhneidi@gmail.com>
2024-02-10 15:23:10 +0100

    updated ST template & snippet

--- templates/data structures/SegmentTree.cpp
+++ templates/data structures/SegmentTree.cpp
@@ -1,23 +1,20 @@
 struct ST {
     struct D {
         ll v = 0;
-
         D() {}
-
-        D(ll x) {
-            v = x;
-        }
-
+        D(ll v) : v(v) {}
         void reset() {
             v = 0;
         }
-
         // combine
         D operator+=(const D &x) {
             v += x.v;
             return *this;
         }
-
+        // accumulating D over a segment of length x
+        D operator*(const int x) {
+            return {v * x};
+        }
         // return true if nothing to propagate
         bool operator!() {
             return !v;
@@ -26,18 +23,15 @@ struct ST {
 
     struct T {
         ll v = 0;
-
         // combine
         T operator+(const T &that) const {
             return {v + that.v};
         }
-
         // combine with lazy
         T operator+=(const D &d) {
             v += d.v;
             return *this;
         }
-
         // the final useful piece of info we're interested in
         // make sure the return type matches the actual type of the returned value
         ll res() {
@@ -48,19 +42,21 @@ struct ST {
     int n, h;
     vector<T> t;
     vector<D> d;
+    vi ln;
 
     ST(int n) {
         this->n = n;
         this->h = 31 - countl_zero(unsigned(n));
         t.resize(n << 1);
+        ln.resize(n << 1);
         d.resize(n);
+        for (int i = 0; i < n; ++i) ln[i + n] = 1;
+        for (int i = n - 1; i; --i) ln[i] = ln[i << 1] + ln[i << 1 | 1];
     }
-
     ST(vi &a) : ST(a.size()) {
         for (int i = 0; i < n; ++i) t[i + n] = T(a[i]);
         for (int i = n - 1; i; --i) calc(i);
     }
-
     void update(int l, int r, D x) {
         l += n, r += n + 1;
         int l0 = l, r0 = r;
@@ -71,7 +67,6 @@ struct ST {
         }
         pull(l0, r0);
     }
-
     T query(int l, int r) {
         l += n, r += n + 1;
         push(l, r);
@@ -87,26 +82,22 @@ struct ST {
     void calc(int i) {
         t[i] = t[i << 1] + t[i << 1 | 1];
     }
-
     void apply(int i, D x) {
-        t[i] += x;
+        t[i] += x * ln[i];
         if (i < n) d[i] += x;
     }
-
     void push(int i) {
         if (!d[i]) return;
         apply(i << 1, d[i]);
         apply(i << 1 | 1, d[i]);
         d[i].reset();
     }
-
     void push(int l, int r) {
         for (int i = h; i; --i) {
             if (l >> i << i != l) push(l >> i);
             if (r >> i << i != r) push(r >> i);
         }
     }
-
     void pull(int l, int r) {
         for (int i = 1; i <= h; ++i) {
             if (l >> i << i != l) calc(l >> i);
