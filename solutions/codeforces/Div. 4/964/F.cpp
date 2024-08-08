1315e10005968142f087d040c6748001a7b5f12b
Jamal Alhneidi <jamal.alhneidi@gmail.com>
2024-08-08 11:52:30 +0200

    added sol cf div4 964 F & G2

---
+++ solutions/codeforces/Div. 4/964/F.cpp
@@ -0,0 +1,201 @@
+/*
+
+Expected Median
+
+https://codeforces.com/contest/1999/problem/F
+
+combinatorics (nck on-the-go trick)
+
+ */
+
+#include "bits/stdc++.h"
+
+#ifdef COTATO
+
+#include "templates/others/dbg.cpp"
+
+#else
+#define dbg(...)
+#define dbgln(...)
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef queue<int> qi;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vector<pll>> vvpll;
+typedef vector<char> vc;
+typedef vector<vector<char>> vvc;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+#define endl '\n'
+#define all(a) a.begin(), a.end()
+#define yes "YES"
+#define no "NO"
+#define spc ' '
+#define INF INT_MAX
+#define LLINF LONG_LONG_MAX
+#define NINF INT_MIN
+#define LLNINF LONG_LONG_MIN
+#define pb push_back
+#define pf push_front
+#define eb emplace_back
+#define F first
+#define S second
+#define cin(a) \
+    for (auto &_x : (a)) cin >> _x
+#define cin2(a)          \
+    for (auto &_x : (a)) \
+        for (auto &__x : _x) cin >> __x
+#define cout(a) \
+    for (auto &_x : (a)) cout << _x << spc
+#define len(l, r) ((r) - (l) + 1)
+#define mid(l, r) ((l) + ((r) - (l)) / 2)
+#define ceil(a, b) (!(a) ? 0 : ((a) - 1) / (b) + 1)
+#define yn(b) ((b) ? yes : no)
+#define between(x, l, r) ((x) >= (l) && (x) <= (r))
+#define between2(i, j, n, m) (between(i, 0, n - 1) && between(j, 0, m - 1))
+
+template <int MOD>
+struct ModInt {
+    int value;
+
+    ModInt(long long v = 0) {
+        value = v % MOD;
+        if (value < 0) value += MOD;
+    }
+
+    ModInt &operator+=(ModInt const &b) {
+        value += b.value;
+        if (value >= MOD) value -= MOD;
+        return *this;
+    }
+
+    ModInt &operator-=(ModInt const &b) {
+        value -= b.value;
+        if (value < 0) value += MOD;
+        return *this;
+    }
+
+    ModInt &operator*=(ModInt const &b) {
+        value = (long long)value * b.value % MOD;
+        return *this;
+    }
+
+    ModInt &operator/=(const ModInt &b) { return *this *= b.inv(); }
+
+    friend ModInt operator+(ModInt a, ModInt const b) { return a += b; }
+
+    friend ModInt operator-(ModInt a, ModInt const b) { return a -= b; }
+
+    friend ModInt operator-(ModInt const a) { return 0 - a; }
+
+    friend ModInt operator*(ModInt a, ModInt const b) { return a *= b; }
+
+    friend ModInt operator/(ModInt a, ModInt const b) { return a /= b; }
+
+    friend ostream &operator<<(std::ostream &os, ModInt const &a) { return os << a.value; }
+
+    friend bool operator==(ModInt const &a, ModInt const &b) { return a.value == b.value; }
+
+    friend bool operator!=(ModInt const &a, ModInt const &b) { return a.value != b.value; }
+
+    ModInt &operator++() {
+        *this += 1;
+        return *this;
+    }
+
+    ModInt operator++(int) {
+        ModInt old = *this;
+        operator++();
+        return old;
+    }
+
+    ModInt &operator--() {
+        *this -= 1;
+        return *this;
+    }
+
+    ModInt operator--(int) {
+        ModInt old = *this;
+        operator--();
+        return old;
+    }
+
+    ModInt pow(long long e) const {
+        if (e < 0) return inv().pow(-e);
+        ModInt a = *this, res = 1;
+        while (e) {
+            if (e & 1) res *= a;
+            a *= a;
+            e >>= 1;
+        }
+        return res;
+    }
+
+    ModInt inv() const { return pow(MOD - 2); }
+};
+
+const int MOD = 1e9 + 7;
+
+typedef ModInt<MOD> mint;
+typedef vector<mint> vmi;
+typedef vector<vmi> vvmi;
+
+vmi f = {1}, inv = {1};
+
+mint nck(int n, int k) {
+    if (k < 0 || k > n) return 0;
+    while (f.size() < n + 1) {
+        f.pb(f.back() * f.size());
+        inv.pb(f.back().inv());
+    }
+    return f[n] * inv[n - k] * inv[k];
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vi a(n);
+    cin(a);
+    int o = count(all(a), 1);
+    int z = n - o;
+    mint ans;
+    for (int i = k / 2 + 1; i <= o; ++i) {
+        ans += nck(o, i) * nck(z, k - i);
+    }
+    cout << ans;
+}
+
+#define MULTI
+
+int main() {
+    fast;
+    int t = 1;
+#ifdef MULTI
+    cin >> t;
+#endif
+    for (int i = 1; i <= t; ++i) {
+        solve();
+        i != t &&cout << endl;
+    }
+}
+/*
+
+
+
+ */
---
+++ solutions/codeforces/Div. 4/964/G2.cpp
@@ -0,0 +1,118 @@
+/*
+
+Ruler (hard version)
+
+https://codeforces.com/contest/1999/problem/G2
+
+ternary search (right half-open interval)
+
+ */
+
+#include "bits/stdc++.h"
+
+#ifdef COTATO
+
+#include "templates/others/dbg.cpp"
+
+#else
+#define dbg(...)
+#define dbgln(...)
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef queue<int> qi;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<ll> vll;
+typedef vector<vector<ll>> vvll;
+typedef vector<bool> vb;
+typedef vector<vector<bool>> vvb;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vector<pll>> vvpll;
+typedef vector<char> vc;
+typedef vector<vector<char>> vvc;
+typedef vector<string> vs;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+#define endl '\n'
+#define all(a) a.begin(), a.end()
+#define yes "YES"
+#define no "NO"
+#define spc ' '
+#define INF INT_MAX
+#define LLINF LONG_LONG_MAX
+#define NINF INT_MIN
+#define LLNINF LONG_LONG_MIN
+#define pb push_back
+#define pf push_front
+#define eb emplace_back
+#define F first
+#define S second
+#define cin(a) \
+    for (auto &_x : (a)) cin >> _x
+#define cin2(a)          \
+    for (auto &_x : (a)) \
+        for (auto &__x : _x) cin >> __x
+#define cout(a) \
+    for (auto &_x : (a)) cout << _x << spc
+#define len(l, r) ((r) - (l) + 1)
+#define mid(l, r) ((l) + ((r) - (l)) / 2)
+#define ceil(a, b) (!(a) ? 0 : ((a) - 1) / (b) + 1)
+#define yn(b) ((b) ? yes : no)
+#define between(x, l, r) ((x) >= (l) && (x) <= (r))
+#define between2(i, j, n, m) (between(i, 0, n - 1) && between(j, 0, m - 1))
+
+void solve() {
+    auto ask = [&](int a, int b) {
+        cout << "? " << a << spc << b << endl
+             << flush;
+        int x;
+        cin >> x;
+        if (x == -1) exit(0);
+        return x;
+    };
+    auto ans = [&](int x) {
+        cout << "! " << x << endl
+             << flush;
+    };
+    int l = 1, r = 1000;
+    while (r - l > 1) {
+        int m1 = (2 * l + r) / 3;
+        int m2 = (l + 2 * r) / 3;
+        int x = ask(m1, m2);
+        if (x == m1 * m2) {
+            l = m2;
+        } else if (x == (m1 + 1) * (m2 + 1)) {
+            r = m1;
+        } else {
+            l = m1, r = m2;
+        }
+    }
+    ans(r);
+}
+
+#define MULTI
+
+int main() {
+    fast;
+    int t = 1;
+#ifdef MULTI
+    cin >> t;
+#endif
+    for (int i = 1; i <= t; ++i) {
+        solve();
+    }
+}
+/*
+
+
+
+ */
